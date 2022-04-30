9dacfc4302f8a7d90f5e4977e9c2736a85a41d87
pragnakasthruri <kpragna25@gmail.com>
2022-04-30 07:36:57 +0530

    Adding the employee payroll form UI

--- employee-payroll/src/App.js
+++ employee-payroll/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import PayrollForm from './components/payroll-form/payroll-form';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Dashboard from './components/dashboard/dashboard';
 
 function App() {
   return (
       <Router>
         <div className="App">
               <Switch>
-                  <Route exact path="/" component={Dashboard}/>
                   <Route exact path="/payroll" component={PayrollForm}/>
               </Switch>
         </div>

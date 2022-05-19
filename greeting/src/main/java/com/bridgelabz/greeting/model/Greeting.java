fdc141f5276a0757a0cc8d8263d5dd4bdb89ea00
pragnakasthruri <kpragna25@gmail.com>
2022-05-19 11:03:51 +0530

    Adding the Greeting controller and greeting class

---
+++ greeting/src/main/java/com/bridgelabz/greeting/model/Greeting.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.greeting.model;
+
+public class Greeting {
+
+    private String message;
+    private String name;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
